2a9383443ecde4d137f5ac26809db7a1ebcf1082
yazid farhaoui <yazid.zibem@gmail.com>
2018-07-27 15:40:03 +0200

    maven prod et lautre

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,23 @@
 package dev;
 
+import java.util.ResourceBundle;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		String val = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(val);
+		System.out.println(asciiArt);
+		
+		String environnement = ResourceBundle.getBundle("application").getString(
+				"environnement");
+				System.out.println("Environnement : " + environnement);
+	}
 }
